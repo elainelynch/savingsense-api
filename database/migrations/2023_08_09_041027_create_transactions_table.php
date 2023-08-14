ae602127550b8742ef71bec59f71ce9a00e4b116
Christine Elaine Lynch <lalainelynch@gmail.com>
2023-08-14 13:44:37 +0800

    added category lists on transaction table

--- database/migrations/2023_08_09_041027_create_transactions_table.php
+++ database/migrations/2023_08_09_041027_create_transactions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->enum('type',['income', 'expense']);
-            $table->enum('category',['food & drink', 'electric bill', 'water bill', 'rent', 'transportation', 'beauty', 'education', 'clothing', 'health', 'income']);
+            $table->enum('category',['income', 'food & drink', 'electric bill', 'water bill', 'rent', 'transportation', 'study', 'beauty', 'health', 'entertainment', 'debt payments', 'personal care', 'gifts and donations', 'insurance', 'miscellaneous', 'utilities', 'pets', 'subscriptions', 'home improvement', 'vacation and travel', 'clothing', 'electronic & gadgets'  ]);
             $table->integer('amount');
             $table->timestamp('date')->nullable();
             $table->unsignedBigInteger('user_id');
